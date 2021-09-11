59fe8246d78341eec4d5ea956679949ce34b1bca
Dios Vo <vtmn1212@gmail.com>
2021-09-11 22:11:59 +0700

    fetch option and its actions

--- src/app/dropdown-search/dropdown-search.component.ts
+++ src/app/dropdown-search/dropdown-search.component.ts
@@ -1,16 +1,24 @@
 import { FocusMonitor } from '@angular/cdk/a11y';
 import { CdkConnectedOverlay } from '@angular/cdk/overlay';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatInput } from '@angular/material/input';
-import { merge, Observable } from 'rxjs';
-import { filter, mapTo } from 'rxjs/operators';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { merge, Observable, Subject } from 'rxjs';
+import { filter, map, mapTo, shareReplay, startWith, takeUntil } from 'rxjs/operators';
+import { State, states } from './drop-down-search.model';
 
 @Component({
   selector: 'app-dropdown-search',
   templateUrl: './dropdown-search.component.html',
   styleUrls: ['./dropdown-search.component.scss']
 })
-export class DropdownSearchComponent implements OnInit {
+export class DropdownSearchComponent implements OnInit, OnDestroy {
+  stateCtrl = new FormControl();
+  filteredStates: Observable<Array<State>>;
+  states: Array<State> = states;
+  isCaseSensitive = false;
+
   showPanel$: Observable<boolean>;
   private isPanelHidden$: Observable<boolean>;
   private isPanelVisible$: Observable<boolean>;
@@ -18,20 +26,55 @@ export class DropdownSearchComponent implements OnInit {
   @ViewChild(MatInput, { read: ElementRef, static: true }) private inputEl: ElementRef<HTMLElement>;
   @ViewChild(CdkConnectedOverlay, { static: true }) private connectedOverlay: CdkConnectedOverlay;
 
+  private destroy$ = new Subject<boolean>();
+
   constructor(
     private focusMonitor: FocusMonitor,
   ) { }
 
   ngOnInit(): void {
+    this.watchForState();
     this.showDropdownPanel();
   }
 
   private showDropdownPanel(): void {
-    this.isPanelHidden$ = this.connectedOverlay.backdropClick.pipe(mapTo(false));
+    this.isPanelHidden$ = this.connectedOverlay.backdropClick.pipe(
+      mapTo(false),
+      takeUntil(this.destroy$)
+    );
     this.isPanelVisible$ = this.focusMonitor.monitor(this.inputEl).pipe(
       filter(focused => !!focused),
-      mapTo(true)
+      mapTo(true),
+      takeUntil(this.destroy$)
     );
     this.showPanel$ = merge(this.isPanelHidden$, this.isPanelVisible$);
+  }
+
+  private watchForState(): void {
+    this.filteredStates = this.stateCtrl.valueChanges
+      .pipe(
+        startWith(''),
+        map(state => state ? this.filterStates(state) : this.states.slice()),
+        shareReplay(),
+        takeUntil(this.destroy$)
+      );
+  }
+
+  private filterStates(value: string): Array<State> {
+    return this.states.filter(state => this.caseCheck(state.name).indexOf(this.caseCheck(value)) === 0);
+  }
+
+  private caseCheck(value: string): string {
+    return this.isCaseSensitive ? value : value.toLowerCase();
+  }
+
+  setCaseSensitive({ checked }: MatSlideToggleChange): void {
+    this.isCaseSensitive = checked;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+    this.destroy$.unsubscribe();
   }
 }
--- src/app/dropdown-search/dropdown-search.module.ts
+++ src/app/dropdown-search/dropdown-search.module.ts
@@ -2,12 +2,14 @@ import { A11yModule } from '@angular/cdk/a11y';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterModule } from '@angular/router';
 import { DropdownSearchComponent } from './dropdown-search.component';
-
 
 @NgModule({
   declarations: [
@@ -15,6 +17,7 @@ import { DropdownSearchComponent } from './dropdown-search.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '',
@@ -28,6 +31,8 @@ import { DropdownSearchComponent } from './dropdown-search.component';
     MatIconModule,
     MatInputModule,
     MatFormFieldModule,
+    MatSlideToggleModule,
+    MatAutocompleteModule,
   ]
 })
 export class DropdownSearchModule { }
