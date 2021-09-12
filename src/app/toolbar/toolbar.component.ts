2a86cf592a3abd590b08fe7e2ec046aa61e887d2
Dios Vo <vtmn1212@gmail.com>
2021-09-12 16:27:26 +0700

    implement: Position Strategy with dialog component

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Overlay, OverlayPositionBuilder } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { Component } from '@angular/core';
+import { CreateDialogComponent } from '../cdk-overlay/create-dialog/create-dialog.component';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent {
 
-  constructor() { }
+  constructor(
+    private overlay: Overlay,
+    private positionBuilder: OverlayPositionBuilder
+  ) { }
 
-  ngOnInit(): void {
+  createDialog(): void {
+    const overlayRef = this.overlay.create({
+      hasBackdrop: true,
+      positionStrategy: this.positionBuilder.global().centerHorizontally().centerVertically()
+    });
+    const dialogPortal = new ComponentPortal(CreateDialogComponent);
+
+    overlayRef.attach(dialogPortal);
+    overlayRef.backdropClick().subscribe(() => overlayRef.detach());
   }
-
 }
--- src/app/toolbar/toolbar.module.ts
+++ src/app/toolbar/toolbar.module.ts
@@ -1,15 +1,17 @@
-import { CommonModule } from '@angular/common';
+import { Overlay } from '@angular/cdk/overlay';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 import { ToolbarComponent } from './toolbar.component';
 @NgModule({
   declarations: [ToolbarComponent],
   imports: [
-    CommonModule,
+    RouterModule,
     MatToolbarModule,
     MatButtonModule
   ],
   exports: [ToolbarComponent],
+  providers: [Overlay]
 })
 export class ToolbarModule { }
