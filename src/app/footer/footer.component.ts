fd8369b4a44b5350647c2faf95fe6301da10808e
Dios Vo <vtmn1212@gmail.com>
2021-09-12 23:00:29 +0700

    Footer (always bottom): detect platform/browser

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Platform } from '@angular/cdk/platform';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+  constructor(readonly platform: Platform) { }
+}
