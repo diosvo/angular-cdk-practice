b842e31c997e343fbf3af75baeb60a67587b72fc
Dios Vo <vtmn1212@gmail.com>
2021-09-12 17:35:14 +0700

    fixed backlog

--- src/app/cdk-overlay/create-dialog/create-dialog.component.ts
+++ src/app/cdk-overlay/create-dialog/create-dialog.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-dialog',
   templateUrl: './create-dialog.component.html',
   styleUrls: ['./create-dialog.component.scss']
 })
-export class CreateDialogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class CreateDialogComponent { }
