e41386cd38d2bc2c42b8d00d158796d5029126b0
Dios Vo <vtmn1212@gmail.com>
2021-09-13 20:02:06 +0700

    Layout: breakpoints observer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,25 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Platform } from '@angular/cdk/platform';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-  constructor(readonly platform: Platform) { }
+export class FooterComponent implements OnInit {
+  isWideScreen$: Observable<boolean>;
+
+  constructor(
+    readonly platform: Platform,
+    readonly breakpoint: BreakpointObserver
+  ) { }
+
+  ngOnInit(): void {
+    this.isWideScreen$ = this.breakpoint
+      .observe(['(min-width: 599.98px)' || Breakpoints.Web])
+      .pipe(map(({ matches }) => matches));
+  }
 }
